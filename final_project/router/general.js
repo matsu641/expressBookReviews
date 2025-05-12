5df0656a3b4027b7fb52098ade4c5bfd549dbe75
Misumi <misumi0921@gmail.com>
2025-05-12 09:57:12 -0400

    Task 1 is completed.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,17 +4,23 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+//res.status(200)はアクセスが成功したよ！というサイン
+//.send(...)の中に入るのはclientにデータを送る関数
 
+//6. Post endpoint
+//username と password をリクエストボディから受け取る
+//同じ username がすでに存在していたらエラーを返す
+//成功したらユーザー情報を保存して成功メッセージを返す
 public_users.post("/register", (req,res) => {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // 1. Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', function (req, res) {
+    return res.status(200).send(JSON.stringify(books, null, 4));
 });
+  
 
 // 2. Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
