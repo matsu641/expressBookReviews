037bb7efa69876ef7805e1e12e8eb86d20f6a978
Misumi <misumi0921@gmail.com>
2025-05-12 09:25:38 -0400

    Figured out tasks

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -10,31 +10,31 @@ public_users.post("/register", (req,res) => {
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
-// Get the book list available in the shop
+// 1. Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
-// Get book details based on ISBN
+// 2. Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
  });
   
-// Get book details based on author
+// 3. Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
-// Get all books based on title
+// 4. Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
-//  Get book review
+// 5. Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
