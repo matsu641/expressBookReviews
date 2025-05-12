e4b88fabb77c3cbe0a10eb48ba639d3edb718464
Misumi <misumi0921@gmail.com>
2025-05-12 10:25:44 -0400

    Task2 is completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,6 +6,8 @@ const public_users = express.Router();
 
 //res.status(200)はアクセスが成功したよ！というサイン
 //.send(...)の中に入るのはclientにデータを送る関数
+//req -> クライアントサイドからのURL
+//res -> 返すオブジェクト
 
 //6. Post endpoint
 //username と password をリクエストボディから受け取る
@@ -17,30 +19,40 @@ public_users.post("/register", (req,res) => {
 });
 
 // 1. Get the book list available in the shop
+//GET http://localhost:5000/でアクセスされたときだけ呼ばれる
 public_users.get('/', function (req, res) {
     return res.status(200).send(JSON.stringify(books, null, 4));
 });
   
-
-// 2. Get book details based on ISBN
+// 2. Get book details based on ISBN（本の番号）
+//GET http://localhost:5000/isbn/:isbnでアクセスされたときだけ呼ばれる
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn //URLからisbnを取得
+  const book = books[isbn]
+
+  if (book) {
+    return res.status(200).send(book);   // 料理（本）を返す
+  } else {
+    return res.status(404).json({ message: "Not Found" }); // 見つからないときの返事
+  }
  });
   
 // 3. Get book details based on author
+//GET http://localhost:5000/author/:authorでアクセスされたときだけ呼ばれる
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // 4. Get all books based on title
+//GET http://localhost:5000/title/:titleでアクセスされたときだけ呼ばれる
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(200).send(book);
 });
 
 // 5. Get book review
+//GET http://localhost:5000/review/:isbnでアクセスされたときだけ呼ばれる
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
